6a3287a61fe2ef5664594fc54f8b03198a23623f
cloudera <cloudera@quickstart.cloudera>
2017-03-17 09:00:39 -0700

    fridayCheckPoint

--- filesInHDFS/ ingestPartitionedTable.hql
+++ filesInHDFS/ ingestPartitionedTable.hql
@@ -1,20 +1,18 @@
 
-/*
-Example:
 
-INSERT INTO TABLE retail.order_details_test PARTITION(trans_date)
-SELECT *
-from retail_stg_test.order_details_test2 AS stg_tbl
-where stg_tbl.trans_date BETWEEN '2017-01-27' AND '2017-01-27';
+---Example:
+--- INSERT INTO TABLE retail.order_details_test PARTITION(trans_date)
+---  SELECT * FROM retail_stg_test.order_details_test2 ;
 
-Parameterized:Needs 6 parameters( finaldb name, stage database name, table name(same for both) partition column, min load date , max load date) */
+---Parameterized:
+---Needs 6 parameters( finaldb name, stage database name, table name(same for both) partition column, min load date , max load date)
+ 
 
 
 SET hive.exec.dynamic.partition.mode=nonstrict;
 
-INSERT INTO TABLE ${target_parquet_db}.${target_table} PARTITION(${partition_column})
+INSERT INTO TABLE ${target_parquet_db}.${target_table} PARTITION (${partition_column})
 SELECT *
-FROM ${target_stg_database}.${target_table} AS stg_tbl
-WHERE stg_tbl.${partition_column} BETWEEN ${min_load_date} AND ${max_load_date};
+FROM ${target_stg_database}.${target_table};
 
 
--- ingest.py
+++ ingest.py
@@ -2,7 +2,8 @@
 """ 
 Main program that runs other modules needed for ingestion.
 CLI arguments : [csv property file ]
-To run this on shell : python ingest.py filepath_of_csv_Variables_file
+To run this on shell : python ingest.py filepath_of_csv_Variables_file, varibleFile, optional[minimumLoad,MaxLoad]
+example python ingest.py config.csv config.txt 2017-01-25 2017-01-26
 """
 
 
@@ -10,18 +11,27 @@ import sys
 from IsarLibrary import getmeta,createhql
 from myLibrary import getTables,createPropertyfile,createTableInHive,runIngestWorkflow
 
-csvFile = str(sys.argv[1])
+print "These are the arguments provided: " , sys.argv[1:]
+csvFile = sys.argv[1]
+varFile=sys.argv[2]
+minload=''
+maxload=''
+if len(sys.argv) > 3:
+	minload=sys.argv[3]
+	maxload=sys.argv[4]
+
+
 
 listOfTablesInfo=getTables(csvFile)
 
 for table in  listOfTablesInfo:
-	"""getmeta(table['src_database'],table['src_table'])
+	getmeta(table['src_database'],table['src_table'],table['src_user'],table['src_pwd'],table['src_host'])
 	createhql(table['src_database'] ,table['src_table'],table['partitioned_tbl'] ,table['partition_column'])
-	createPropertyfile(table)
-	createTableInHive(table['src_table'])"""
+	createPropertyfile(table,varFile,minload,maxload)
+	"""createTableInHive(table['src_table'])
 	runIngestWorkflow(table['src_table'])
 	
-	#checkForCompletion
+	#checkForCompletion"""
     
 	
 	
